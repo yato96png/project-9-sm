f977ab138b58d836fd6d3a9f03ef9031b5c66180
vizal123 <yato9png@gmail.com>
2025-01-29 13:27:50 +0500

    Create html.js

---
+++ html.js
@@ -0,0 +1,12 @@
+function createCounter() {
+  let count = 0; 
+  return function() {
+    count += 1;
+    return count;
+  };
+}
+
+const counter = createCounter();
+console.log(counter()); 
+console.log(counter()); 
+console.log(counter()); 
